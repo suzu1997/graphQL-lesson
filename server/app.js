83f4d4f7a46eef31831e653e6f2c8d8e067371a1
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-02-18 15:39:23 +0900

    express-graphqlを用いたミドルウェア作成の準備

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+// express-graphqlからgraphqlHTTPをimport
+const graphqlHTTP = require('express-graphql');
 
 // expressのインスタンスを生成
 // アプリケーション構築に必要な機能を提供する
 const app = express();
+
+// 一つのエンドポイントでデータのやりとりを行うためにミドルウェアを作成
+// 第一引数にパス、第二引数にミドルウェアのハンドラー関数
+app.use('/graphql', graphqlHTTP({
+  // schemaはgraphqlのスキーマを定義
+
+}))
 
 // listen関数でサーバーを起動
 // 第一引数にポート番号、第二引数にコールバック関数
